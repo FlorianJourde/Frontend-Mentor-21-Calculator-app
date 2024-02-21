d36e6b02dcd7a42abc163135dafd8d40c5ac3021
Florian Jourde <jourdeflorian@gmail.com>
2024-02-21 22:58:54 +0100

    Try different logic things

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,55 +1,160 @@
-import { useEffect, useState, MouseEvent } from 'react'
+import { useEffect, useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
 
+// let storedValues: string[] = [];
+// let currentValues: string[] = [];
+
 function Calculator() {
-  const [value, setValue] = useState('0')
-  // const useEffect
-  useEffect(() => {
+  const [value, setValue] = useState<string>('0');
+  const [storedValues, setStoredValues] = useState<string[]>([]);
+  const [endNumberTyping, setEndNumberTyping] = useState<boolean>(true)
 
-  })
+  // useEffect(() => {
+  //   console.log(storedValues);
 
-  class Calculate {
-    reset() {
+  //   // action on update of movies
+  // }, [storedValues]);
 
-    }
+  // useEffect(() => { console.log(value), [value] });
+  // useEffect(() => { console.log(storedValues), [storedValues] });
+  // useEffect(() => { console.log(endNumberTyping), [endNumberTyping] });
 
-    add(a: number, b: number) {
 
-    }
+  // console.log(storedValues);
 
-    substract(a: number, b: number) {
+  // console.log(6*(3+6));
 
-    }
 
-    multiply(a: number, b: number) {
+  function storeResult() {
+    // setValue('0');
+    setStoredValues(currentValues => [...currentValues, value]);
+    // console.log(storedValues);
 
-    }
+  }
 
-    divide(a: number, b: number) {
-
+  function calculate(operator: string) {
+    // console.log(storedValues);
+    // const sum = storedValues.filter(acc, value => )
+    switch (operator) {
+      case 'add':
+        // console.log('+');
+        // console.log(storedValues.length > 0);
+        // if (storedValues.length > 0) {
+        // const sum = storedValues.reduce(
+        //   (acc, value) => parseFloat(acc) + parseFloat(value),
+        //   value,
+        // );
+        // // setStoredValues([sum.toString()]);
+        // // setValue(sum.toString())
+        // // console.log(sum);
+        // }
+        // const sum = storedValues.reduce(
+        //   (acc, value) => acc + parseInt(value),
+        //   0,
+        // );
+        // console.log(storedValues);
+        break;
+      case 'substract':
+        console.log('-');
+        break;
+      case 'divide':
+        console.log('/');
+        break;
+      case 'multiply':
+        console.log('*');
+        break;
     }
   }
 
+  function reset() {
+    setValue('0');
+    setStoredValues([])
+    // storedValues = [];
+  }
+
+  function add(a: number, b: number) { }
+
+  function substract(a: number, b: number) { }
+
+  function multiply(a: number, b: number) { }
+
+  function divide(a: number, b: number) { }
+
   const handleClick = (e: any) => {
     const clickedButton = (e.currentTarget as HTMLButtonElement);
-    const isNumeric = clickedButton.getAttribute('data-type') === 'numeric'
+    const calculatorFunctionType = clickedButton.getAttribute('data-type')
+    const clickedButtonValue = clickedButton.value
+    // console.log(calculatorFunctionType);
 
-    if (isNumeric) {
-      // const currentValue = parseInt(clickedButton.value);
-      updateValue(clickedButton.value);
-    } else {
+    switch (calculatorFunctionType) {
+      case 'numeric':
+        setEndNumberTyping(false);
+        updateValue(clickedButton.value);
 
+      //     if (storedValues.length > 0) {
+      //   setValue(storedValues[0])
+      //   updateValue(clickedButton.value);
+
+      // }
+
+        // storedValues.length === 0 ? setValue('0') : setValue(storedValues[0])
+        // if (storedValues.length > 0) {
+        //   setValue(storedValues[0])
+        //   updateValue(clickedButton.value);
+
+        // }
+
+        // storedValues.length > 0 ? setValue('0') : setValue(storedValues[0])
+
+
+        break;
+      case 'operator':
+        setEndNumberTyping(true);
+        // setValue(storedValues[0])
+        // storedValues.length === 0 ? 0 : storedValues[0]
+        storeResult();
+        // storedValues.length === 0 ? 0 : storedValues[0]
+
+        // storedValues.length === 0 ? setValue('0') : setValue(storedValues[0])
+        // console.log(storedValues.length);
+
+        // console.log(storedValues);
+
+        // updateValue('0');
+        // console.log(clickedButtonValue);
+        calculate(clickedButtonValue);
+        break;
+
+      case 'function':
+        if (clickedButtonValue === 'Del') {
+        } else if (clickedButtonValue === 'Reset') {
+          reset();
+        }
+        break;
     }
   };
 
   const updateValue = (number: any) => {
+    // setEndNumberTyping(false);
     let newValue: string;
 
-    value === '0' ? newValue = number : newValue = value.concat('', number)
-
-    if (value.includes('.') && number === '.') return;
+    console.log(number);
+    console.log(value);
+    console.log(storedValues);
+    
+    
+    if (storedValues.length > 0) {
+      setValue('0')
+    }
+      
+      value === '0' ? newValue = number : newValue = value.concat('', number)
+      
+      if (value.includes('.') && number === '.') return;
+      
+      console.log(value);
+      console.log(newValue);
 
     setValue(newValue)
   }
@@ -85,20 +190,20 @@ function Calculator() {
           <button value="7" onClick={handleClick} data-type="numeric" className="btn">7</button>
           <button value="8" onClick={handleClick} data-type="numeric" className="btn">8</button>
           <button value="9" onClick={handleClick} data-type="numeric" className="btn">9</button>
-          <button value="Del" onClick={handleClick} data-type="operator" className="btn btn-blue">Del</button>
+          <button value="Del" onClick={handleClick} data-type="function" className="btn btn-blue">Del</button>
           <button value="4" onClick={handleClick} data-type="numeric" className="btn">4</button>
           <button value="5" onClick={handleClick} data-type="numeric" className="btn">5</button>
           <button value="6" onClick={handleClick} data-type="numeric" className="btn">6</button>
-          <button value="+" onClick={handleClick} data-type="operator" className="btn">+</button>
+          <button value="add" onClick={handleClick} data-type="operator" className="btn">+</button>
           <button value="1" onClick={handleClick} data-type="numeric" className="btn">1</button>
           <button value="2" onClick={handleClick} data-type="numeric" className="btn">2</button>
           <button value="3" onClick={handleClick} data-type="numeric" className="btn">3</button>
-          <button value="-" onClick={handleClick} data-type="operator" className="btn">-</button>
+          <button value="substract" onClick={handleClick} data-type="operator" className="btn">-</button>
           <button value="." onClick={handleClick} data-type="numeric" className="btn">.</button>
           <button value="0" onClick={handleClick} data-type="numeric" className="btn">0</button>
-          <button value="/" onClick={handleClick} data-type="operator" className="btn">/</button>
-          <button value="*" onClick={handleClick} data-type="operator" className="btn">x</button>
-          <button value="Reset" onClick={handleClick} data-type="operator" className="btn btn-blue col-span-2">Reset</button>
+          <button value="divide" onClick={handleClick} data-type="operator" className="btn">/</button>
+          <button value="multiply" onClick={handleClick} data-type="operator" className="btn">x</button>
+          <button value="Reset" onClick={handleClick} data-type="function" className="btn btn-blue col-span-2">Reset</button>
           <button value="=" onClick={handleClick} data-type="operator" className="btn btn-red col-span-2">=</button>
         </div>
       </div>
