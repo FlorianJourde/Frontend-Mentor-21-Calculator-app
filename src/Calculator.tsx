03958715ddb037d514d52f16db7576b5fc50aa70
Florian Jourde <f.jourde@facil-iti.com>
2024-02-22 18:03:04 +0100

    Enhance calculate logic

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,37 +1,25 @@
 import { useEffect, useState } from 'react'
 
-let storeValue: number = 0;
-let endTypingNumber: boolean = false;
-let currentTypingNumber: string = '0';
-
-function Calculator() {
+export default function Calculator() {
   const [total, setTotal] = useState<number>(0);
   const [operator, setOperator] = useState<string>('');
-  const [value, setValue] = useState<string>('0');
+  const [currentValue, setCurrentValue] = useState<string>('0');
 
-  useEffect(() => { console.log(value), [value] });
-
-  function storeResult() {
-    storeValue = storeValue + parseFloat(value);
-  }
-
-  function reinitDisplay() { }
-
-  function calculate(operator: string) {
-    switch (operator) {
-      case 'add':
-        break;
-      case 'substract':
-        break;
-      case 'divide':
-        break;
-      case 'multiply':
-        break;
-    }
-  }
+  // useEffect(() => { console.log(total); }, [total]);
+  // useEffect(() => { console.log(operator); }, [operator]);
+  // useEffect(() => { console.log(currentValue); }, [currentValue]);
 
   function reset() {
-    setValue('0');
+    setCurrentValue('0');
+    setOperator('');
+    setTotal(0);
+  }
+
+  function animateButton(clickedButton: HTMLButtonElement) {
+    clickedButton.classList.add('clicked');
+    setTimeout(function () {
+      clickedButton.classList.remove("clicked");
+    }, 150);
   }
 
   const handleClick = (e: any) => {
@@ -39,28 +27,73 @@ function Calculator() {
     const calculatorFunctionType = clickedButton.getAttribute('data-type')
     const clickedButtonValue = clickedButton.value
 
+    animateButton(clickedButton);
+
     switch (calculatorFunctionType) {
       case 'numeric':
-        updateValue(clickedButton.value);
+        updateValue(clickedButtonValue);
         break;
       case 'operator':
-        setValue(storeValue.toString())
-        storeResult();
+        updateOperation();
+        setOperator(clickedButtonValue);
+        setCurrentValue('0');
         break;
       case 'function':
         if (clickedButtonValue === 'Del') {
+          setCurrentValue(currentValue.slice(0, -1));
         } else if (clickedButtonValue === 'Reset') {
           reset();
+        }
+        else if (clickedButtonValue === '=') {
+          updateOperation(true);
+          // setOperator(clickedButtonValue);
+          // setTotal(0);
+          // setCurrentValue(total.toString());
         }
         break;
     }
   };
 
-  const updateValue = (number: any) => {
-    currentTypingNumber = currentTypingNumber.concat('', number)
-    setValue(currentTypingNumber)
-    while (currentTypingNumber.charAt(0) == '0') currentTypingNumber = currentTypingNumber.substring(1);
-    if (value.includes('.') && number === '.') return;
+  const updateValue = (clickedNumber: any) => {
+    if (currentValue.includes('.') && clickedNumber === '.') return;
+
+    if (currentValue.charAt(0) === '0') {
+      setCurrentValue(currentValue.substring(1).concat('', clickedNumber))
+    } else {
+      setCurrentValue(currentValue.concat('', clickedNumber))
+    }
+  }
+
+  function calculate(a: number, str: string, b: number) {
+    switch (str) {
+      case '+': return a + b
+      case '-': return a - b
+      case '/': return a / b
+      case '*': return a * b
+      default: return 0
+    }
+  }
+
+  function updateOperation(showResult = false) {
+    const resolveCalc = calculate(total, operator, parseFloat(currentValue))
+
+    if (operator.length > 0) {
+      // console.log(operator);
+      setTotal(resolveCalc)
+
+      if (showResult) {
+        setTotal(0);
+        setCurrentValue(resolveCalc.toString());
+      }
+    }
+    else {
+      setTotal(parseFloat(currentValue))
+    }
+
+    // if (clickedButtonValue === '=') {
+    //   setCurrentValue(currentValue.toString())
+    // } else {
+    // }
   }
 
   return (
@@ -88,8 +121,11 @@ function Calculator() {
           </div>
         </div> */}
         <div className="display flex flex-col text-right p-8 rounded-lg justify-end bg-[#181f32] text-white font-bold">
-          <h3 className="text-2xl">{total} {operator}</h3>
-          <h2 className='text-3xl'>{value}</h2>
+          {/* {isTotal()} */}
+          {/* {total > 0 && <h3 className="text-2xl">{total} {operator}</h3>} */}
+          <h3 className={'text-2xl transition-all' + ' ' + (total === 0 ? 'opacity-0 translate-y-2' : 'translate-y-1')}>{total} {operator}</h3>
+          {/* {{total > 0 ? <h3 className="text-2xl">{total} {operator}</h3>}} */}
+          <h2 className='text-3xl'>{currentValue}</h2>
         </div>
         <div className="keyboard bg-[#252d44] p-8 rounded-lg grid grid-cols-4 gap-5">
           <button value="7" onClick={handleClick} data-type="numeric" className="btn">7</button>
@@ -99,21 +135,19 @@ function Calculator() {
           <button value="4" onClick={handleClick} data-type="numeric" className="btn">4</button>
           <button value="5" onClick={handleClick} data-type="numeric" className="btn">5</button>
           <button value="6" onClick={handleClick} data-type="numeric" className="btn">6</button>
-          <button value="add" onClick={handleClick} data-type="operator" className="btn">+</button>
+          <button value="+" onClick={handleClick} data-type="operator" className="btn">+</button>
           <button value="1" onClick={handleClick} data-type="numeric" className="btn">1</button>
           <button value="2" onClick={handleClick} data-type="numeric" className="btn">2</button>
           <button value="3" onClick={handleClick} data-type="numeric" className="btn">3</button>
-          <button value="substract" onClick={handleClick} data-type="operator" className="btn">-</button>
+          <button value="-" onClick={handleClick} data-type="operator" className="btn">-</button>
           <button value="." onClick={handleClick} data-type="numeric" className="btn">.</button>
           <button value="0" onClick={handleClick} data-type="numeric" className="btn">0</button>
-          <button value="divide" onClick={handleClick} data-type="operator" className="btn">/</button>
-          <button value="multiply" onClick={handleClick} data-type="operator" className="btn">x</button>
+          <button value="/" onClick={handleClick} data-type="operator" className="btn">/</button>
+          <button value="*" onClick={handleClick} data-type="operator" className="btn">x</button>
           <button value="Reset" onClick={handleClick} data-type="function" className="btn btn-blue col-span-2">Reset</button>
-          <button value="=" onClick={handleClick} data-type="operator" className="btn btn-red col-span-2">=</button>
+          <button value="=" onClick={handleClick} data-type="function" className="btn btn-red col-span-2">=</button>
         </div>
       </div>
     </div>
   )
 }
-
-export default Calculator
